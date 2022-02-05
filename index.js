9721497a5092fe9d076e0a4c4fb878489b533e45
Abdul Awal Nadim <nadim.ice.nstu@gmail.com>
2022-02-05 21:05:42 +0600

    Parsing Request

--- index.js
+++ index.js
@@ -6,29 +6,51 @@
  *
  */
 // dependencies
-const http = require('http')
-
+const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 // app object - module scaffolding
-const app = {}
+const app = {};
 
 // configuration
 app.config = {
   port: 3000,
-}
+};
 
 // create server
 app.createServer = () => {
-  const server = http.createServer(app.handleReqRes)
+  const server = http.createServer(app.handleReqRes);
   server.listen(app.config.port, () => {
-    console.log(`listening to port ${app.config.port}`)
-  })
-}
+    console.log(`listening to port ${app.config.port}`);
+  });
+};
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-  //response handle
-  res.end('Hello nadim')
-}
+  //request handling
+  //get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toLowerCase();
+  const queryStrimgObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const decoder = new StringDecoder('utf-8');
+  let realData = '';
+
+  req.on('data', (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on('end', () => {
+    realData += decoder.end();
+
+    console.log(realData);
+    //response handle
+    res.end('Hello world');
+  });
+};
 
 // start the server
-app.createServer()
+app.createServer();
